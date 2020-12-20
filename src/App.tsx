00f412a9e38f3e03497ae030053a641dcba443b2
serbes85 <beskrovnyisergei@mail.ru>
2020-12-20 19:50:00 +0300

    added pages Works, About, Blog & added Route

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,21 @@
 import React from "react";
+import { BrowserRouter, Switch, Route } from "react-router-dom";
 import { Welcome } from "./pages/Welcome/Welcome";
+import { Works } from "./pages/Works/Works";
+import { About } from "./pages/About/About";
+import { Blog } from "./pages/Blog/Blog";
 
 const App: React.FC = () => {
-  return <Welcome />;
+  return (
+    <BrowserRouter>
+      <Switch>
+        <Route component={Welcome} path="/" exact />
+        <Route component={Works} path="/works" />
+        <Route component={About} path="/about" />
+        <Route component={Blog} path="/blog" />
+      </Switch>
+    </BrowserRouter>
+  );
 };
 
 export default App;
--- src/components/Intro/Intro.tsx
+++ src/components/Intro/Intro.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { useHistory } from "react-router-dom";
 import { IntroProps } from "./interfaces";
 import { Socials } from "../Socials/Socials";
 import { Person } from "../Person/Person";
@@ -6,6 +7,7 @@ import { Button } from "../Button/Button";
 import styles from "./Intro.module.scss";
 
 export const Intro: React.FC<IntroProps> = ({ className }) => {
+  const history = useHistory();
   return (
     <div className={className}>
       <div className={styles.hero}>
@@ -13,9 +15,21 @@ export const Intro: React.FC<IntroProps> = ({ className }) => {
         <Socials />
       </div>
       <nav className={styles.nav}>
-        <Button buttonText="Мои работы" appearance="borderRadiusLeft" />
-        <Button buttonText="Обо мне" appearance="borderRight" />
-        <Button buttonText="Блог" appearance="borderRadiusRight" />
+        <Button
+          buttonText="Мои работы"
+          appearance="borderRadiusLeft"
+          onClick={() => history.push("/works")}
+        />
+        <Button
+          buttonText="Обо мне"
+          appearance="borderRight"
+          onClick={() => history.push("/about")}
+        />
+        <Button
+          buttonText="Блог"
+          appearance="borderRadiusRight"
+          onClick={() => history.push("/blog")}
+        />
       </nav>
     </div>
   );
